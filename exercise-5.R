a3a82f4f48b809d6e49ba771eafc15b6135fa1a2
Julia Pettere <jpettere77@gmail.com>
2017-01-17 13:57:48 -0800

    Add fixed to exercise5

--- exercise-5.R
+++ exercise-5.R
@@ -12,15 +12,15 @@ words.of.the.day <- paste0('"', words, '"', "is the word of the day!")
 # Create a vector `a.f.words` which are the elements in `words` that start with "a" through "f"
 # Hint: use a comparison operator to see if the word comes before "f" alphabetically!
 # Tip: make sure all the words are lower-case, and only consider the first letter of the word!
-a.f.words <- words[str_to_lower(words) < 'f']
+a.f.words <- words[substr(words, 1,1) <= 'f']
 
 # Create a vector `g.m.words` which are the elements in `words` that start with "g" through "m"
-g.m.words <- words[str_to_lower(words) >= 'g' & str_to_lower(words) <= 'm']
+g.m.words <- words[substr(words, 1, 1) >= 'g' & substr(words, 1, 1) <= 'm']
 
 # Define a function `WordBin` that takes in three arguments: a vector of words, and two letters. The function
 # should return a vector of words that go between those letters alphabetically.
 WordBin <- function(words, l1, l2) {
-  return(words[str_to_lower(words) >= l1 & str_to_lower(words) <= l2])  
+  return(words[substr(words, 1, 1) >= l1 & substr(words, 1, 1) <= l2])  
 }
 
 # Use your `WordBin` function to determine which of your words start with "e" through "q"
